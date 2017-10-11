2101e674c653665a3ea4c6f3bb483f8344b5983a
Athman Gude <athmangude@gmail.com>
2017-10-11 16:25:29 +0300

    setup redux

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,19 @@
 import Link from 'next/link';
+import withRedux from 'next-redux-wrapper';
 
-export default () => (
-  <div>
-    Hello Next <Link href="/about"><a>About</a></Link>
-  </div>
-);
+import configureStore from './store';
+
+let Index = (props, context) => {
+  console.log(props);
+  return (
+    <div>
+      Hello Next <Link href="/about"><a>About</a></Link>
+    </div>
+  );
+}
+
+Index = withRedux(configureStore, (state) => ({
+  ...state
+}))(Index);
+
+export default Index;
